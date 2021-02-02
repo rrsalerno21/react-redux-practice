e36fcb8de7190ecf70f80ab306c49f94045f6ab2
Rocky Salerno <rsalerno@pinsadvantage.com>
2021-02-01 19:22:42 -0800

    created store w/ counter reducer

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION INCREMENT
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+
+// Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
