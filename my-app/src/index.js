d95e369aa6039983afc416d6cc466d3b59c93549
Rocky Salerno <rsalerno@pinsadvantage.com>
2021-02-02 08:05:10 -0800

    added redux store and reducers directory

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,45 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore} from 'redux';
+import allReducer from './reducers';
+import {Provider} from 'react-redux';
 
-
-// STORE -> GLOBALIZED STATE
-
-// ACTION INCREMENT
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-// REDUCER
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-}
-
-let store = createStore(counter);
-
-// Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-// DISPATCH
-store.dispatch(increment());
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
